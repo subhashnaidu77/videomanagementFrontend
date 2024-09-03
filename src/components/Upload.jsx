7c7423d528b6f82cdf3739f8448938214af16ff6
subhashnaidu77 <55914212+subhashnaidu77@users.noreply.github.com>
2024-09-04 01:00:07 +0530

    Update Upload.jsx

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -135,7 +135,7 @@ const Upload = ({ setOpen }) => {
 
   const handleUpload = async (e)=>{
     e.preventDefault();
-    const res = await axios.post("/videos", {...inputs, tags})
+    const res = await axios.post("https://videoback-7csk.onrender.com/api/videos", {...inputs, tags})
     setOpen(false)
     res.status===200 && navigate(`/video/${res.data._id}`)
   }
